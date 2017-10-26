bc6ebb550a8b7bd22ac308e75ae477eca48c5dc0
radlinsk <radlinsk@student.agh.edu.pl>
2017-10-26 13:53:56 +0200

    w intellij projekty ostatnie

---
+++ test/com/company/lab2/MatrixTest.java
@@ -0,0 +1,106 @@
+package com.company.lab2;
+
+import static org.junit.Assert.*;
+
+public class MatrixTest {
+    @org.junit.Test
+    public void getRows() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getCols() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getData() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setRows() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setCols() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setData() throws Exception {
+    }
+
+    @org.junit.Test
+    public void asArray() throws Exception {
+    }
+
+    @org.junit.Test
+    public void get() throws Exception {
+    }
+
+    @org.junit.Test
+    public void set() throws Exception {
+    }
+
+    @org.junit.Test
+    public void testToString() throws Exception {
+    }
+
+    @org.junit.Test
+    public void reshape() throws Exception {
+    }
+
+    @org.junit.Test
+    public void shape() throws Exception {
+    }
+
+    @org.junit.Test
+    public void add() throws Exception {
+    }
+
+    @org.junit.Test
+    public void sub() throws Exception {
+    }
+
+    @org.junit.Test
+    public void mul() throws Exception {
+    }
+
+    @org.junit.Test
+    public void div() throws Exception {
+    }
+
+    @org.junit.Test
+    public void dot() throws Exception {
+    }
+
+    @org.junit.Test
+    public void frobenius() throws Exception {
+    }
+
+    @org.junit.Test
+    public void add1() throws Exception {
+    }
+
+    @org.junit.Test
+    public void sub1() throws Exception {
+    }
+
+    @org.junit.Test
+    public void mul1() throws Exception {
+    }
+
+    @org.junit.Test
+    public void div1() throws Exception {
+    }
+
+    @org.junit.Test
+    public void eye() throws Exception {
+    }
+
+    @org.junit.Test
+    public void random() throws Exception {
+    }
+
+    @org.junit.Test
+    public void main() throws Exception {
+    }
+
+}
