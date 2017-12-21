27b740bee3ec51b6350cdff0e5cb3da1494b57fa
radlinsk <radlinsk@student.agh.edu.pl>
2017-12-21 13:59:45 +0100

    lab10 laboratorium

---
+++ lab10/src/ChristmasTree/Bubble.java
@@ -0,0 +1,56 @@
+package ChristmasTree;
+
+import java.awt.*;
+
+public class Bubble implements XmasShape {
+    public void setScale(double scale) {
+        this.scale = scale;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setLineColor(Color lineColor) {
+        this.lineColor = lineColor;
+    }
+
+    public void setFillColor(Color fillColor) {
+        this.fillColor = fillColor;
+    }
+
+    int x;
+    int y;
+    double scale;
+    Color lineColor;
+    Color fillColor;
+
+    Bubble(){
+        x = 0;
+        y = 0;
+        scale = 1;
+        lineColor = Color.MAGENTA;
+        fillColor = Color.CYAN;
+    }
+
+
+    @Override
+    public void render(Graphics2D g2d) {
+        // ustaw kolor wypełnienia
+        g2d.setColor(fillColor);
+        g2d.fillOval(0,0,100,100);
+        // ustaw kolor obramowania
+        g2d.setColor(lineColor);
+        g2d.drawOval(0,0,100,100);
+    }
+
+    @Override
+    public void transform(Graphics2D g2d) {
+        g2d.translate(x,y);
+        g2d.scale(scale,scale);
+    }
+}
